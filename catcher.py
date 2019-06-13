beaeeef1aeea1601cf65ad5b23fce892d9e9dfea
Mahisaurus-Rex <mahipasarkar@gmail.com>
2019-06-13 10:39:17 -0400

    oop controls

--- catcher.py
+++ catcher.py
@@ -10,22 +10,34 @@ done = False
 p1_x=30
 p1_y= screen.get_height()-60
 #make player
-p1=pygame.draw.rect(screen, (255,255,255), pygame.Rect(p1_x,p1_y,60,60))
+class Player:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def moveLeft(self):
+        if self.x>0:
+            self.x-=2
+    def moveRight(self):
+        if self.x<screen.get_width()-60: 
+            self.x+=2
+    def draw(self):
+        pygame.draw.rect(screen, (255,255,255), pygame.Rect(self.x,self.y,60,60))
+
 class Egg:
     def spawn(self):
         self.x=random.randint(0,screen.get_width()-30)
-        self.y
-
+        #self.y
+p1 = Player(p1_x,p1_y)
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     pressed=pygame.key.get_pressed()
     #movement
-    if pressed[pygame.K_a] and p1_x>0: p1_x-=2
-    if pressed[pygame.K_d] and p1_x<screen.get_width()-60: p1_x+=2
+    if pressed[pygame.K_a] : p1.moveLeft()
+    if pressed[pygame.K_d] : p1.moveRight()
 
     screen.fill((0,0,0))
     #screen.blit(score, ((screen.get_width()/2)-20,0))
-    p1=pygame.draw.rect(screen, (255,255,255), pygame.Rect(p1_x,p1_y,60,60))
+    p1.draw()
     pygame.display.flip()
